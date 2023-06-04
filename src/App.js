ac4ad903b7990405b7cef1194ee0a46102f28e22
Irfnmdlptra <bangasrat@gmail.com>
2023-06-04 14:45:32 +0800

    siapa

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends React.Component {
         <div className="row">
           <div className="col-12">
             <h1>FEEDUITEN APPS</h1>
+            <hr />
+            <h4>Rp.1.500.00,-</h4>
+            <span>Sisa uang kamu tersisa 75% lagi</span>
           </div>
         </div>
       </div>
