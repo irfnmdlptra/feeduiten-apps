b353154b6b41f0d05af62ba7dcd6de84edbc8e04
Irfnmdlptra <bangasrat@gmail.com>
2023-06-04 14:31:34 +0800

    e

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hallo</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>hallo</h1>
+      </div>
+    );
+  }
 }
-
 export default App;
