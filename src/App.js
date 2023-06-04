44bd7bde610b34a6dc6144d968eb22f62598ad46
Irfnmdlptra <bangasrat@gmail.com>
2023-06-04 14:44:19 +0800

    d

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        
+        <div className="row">
+          <div className="col-12">
+            <h1>FEEDUITEN APPS</h1>
+          </div>
+        </div>
       </div>
     );
   }
