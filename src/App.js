50ce3c1ca6c1e27d82bdaeefed7b414266f25e94
Irfnmdlptra <bangasrat@gmail.com>
2023-06-05 10:51:21 +0800

    sd

--- src/App.js
+++ src/App.js
@@ -51,10 +51,10 @@ class App extends React.Component {
           <div className="col-12 d-flex align-items-center justify-content-between">
             <h4>Ringkasan Transaksi</h4>
             <div className="wrapper-button d-flex">
-              <button className="button btn-ungu">
+              <button className="button btn-ungu px-3 py-2 me-2">
                 Pemasukan <i className="bi bi-patch-plus-fill"></i>
               </button>
-              <button className="button btn-pink">
+              <button className="button btn-pink px-3 py-2">
                 Pengeluaran <i className="bi bi-patch-minus-fill"></i>
               </button>
             </div>
