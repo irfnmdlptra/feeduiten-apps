d90ff072fc4a7ad0f177fcc143d31f4b6b2d37f3
Irfnmdlptra <bangasrat@gmail.com>
2023-06-07 08:04:31 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 class App extends React.Component {
   constructor() {
@@ -36,7 +38,7 @@ class App extends React.Component {
             <h1 className="fw-bold">FEEDUITEN APPS</h1>
             <hr className="w-75 mx-auto" />
             <h2 className="fw-bold">Rp. {this.state.sisaUang} ,-</h2>
-            <span className="title-sm">Sisa uang kamu tersisa 75% lagi</span>
+            <span className="title-sm">Sisa uang kamu tersisa {this.state.persentaseUang}% lagi</span>
           </div>
         </div>
 
@@ -74,23 +76,19 @@ class App extends React.Component {
           <div className="col-12 d-flex align-items-center justify-content-between">
             <h4>Ringkasan Transaksi</h4>
             <div className="wrapper-button d-flex">
-              <button className="button btn-ungu px-3 py-2 me-2 box-sh">
-                Pemasukan <i className="bi bi-plus-circle ms-1"></i>
-              </button>
-              <button className="button btn-pink px-3 py-2 box-sh">
-                Pengeluaran <i className="bi bi-dash-circle-dotted ms-1"></i>
-              </button>
+              <ModalCreate variant="button btn-ungu px-3 py-2 me-2 box-sh" text="Pemasukan" icon="bi bi-plus-circle ms-1" modalheading="Tambahkan Pemasukan" />
+              <ModalCreate variant="button btn-pink px-3 py-2 box-sh" text="Pengeluaran" icon="bi bi-dash-circle-dotted ms-1" modalheading="Tambahkan Pengeluaran" />
             </div>
           </div>
         </div>
 
         <div className="row mt-4">
-          {this.state.summary.map((sum) => {
+          {this.state.summary.map((sum, index) => {
             return (
-              <div className="col-12 d-flex mb-3 justify-content-between align-items-center">
+              <div key={index} className="col-12 d-flex mb-3 justify-content-between align-items-center">
                 <div className="d-flex align-items-center">
-                  <div className={sum.category === 'IN' ? 'icon-wrapper-in box-sh' : 'icon-wrapper-out box-sh'}>
-                    <i className={sum.category === 'IN' ? 'bi bi-wallet2' : 'bi bi-bag-dash'} aria-hidden="true"></i>
+                  <div className={sum.category === "IN" ? "icon-wrapper-in box-sh" : "icon-wrapper-out box-sh"}>
+                    <i className={sum.category === "IN" ? "bi bi-wallet2" : "bi bi-bag-dash"} aria-hidden="true"></i>
                   </div>
                   <div className="transaction ms-3 d-flex flex-column">
                     <h5>{sum.deskripsi} </h5>
@@ -98,7 +96,7 @@ class App extends React.Component {
                   </div>
                 </div>
                 {/* Conditional Rendering  untuk mengendalikan tampilan elemen di halaman berdasarkan kondisi tertentu.  */}
-                <h6 className={sum.category === 'IN' ? 'money-in' : 'money-out'} >Rp. {sum.nominal} </h6>
+                <h6 className={sum.category === "IN" ? "money-in" : "money-out"}>Rp. {sum.nominal} </h6>
               </div>
             );
           })}
@@ -107,4 +105,50 @@ class App extends React.Component {
     );
   }
 }
+
+class ModalCreate extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleClose() {
+    this.setState({
+      show: false,
+    });
+  }
+
+  handleShow() {
+    this.setState({
+      show: true,
+    });
+  }
+  render() {
+    return (
+      <>
+        <button onClick={this.handleShow} className={this.props.variant}>
+          {this.props.text} <i className={this.props.icon}></i>
+        </button>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.modalheading} </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+}
+
 export default App;
