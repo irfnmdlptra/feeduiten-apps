4d86afe79d733f4a5b7c938db7e6572d05d65b8c
Irfnmdlptra <bangasrat@gmail.com>
2023-06-12 14:25:42 +0800

    .

--- src/App.js
+++ src/App.js
@@ -42,11 +42,15 @@ class App extends React.Component {
   }
 
   fnHitung() {
-    let dataUangIN = this.state.summary.filter((item) => item.category === "IN");
+    let dataUangIN = this.state.summary.filter(
+      (item) => item.category === "IN"
+    );
     let nominalUangIN = dataUangIN.map((item) => item.nominal);
     let jumlahUangIN = nominalUangIN.reduce((total, num) => total + num);
 
-    let dataUangOUT = this.state.summary.filter((item) => item.category === "OUT");
+    let dataUangOUT = this.state.summary.filter(
+      (item) => item.category === "OUT"
+    );
     let nominalUangOUT = dataUangOUT.map((item) => item.nominal);
     let jumlahUangOUT = nominalUangOUT.reduce((total, num) => total + num);
     let persenUang = ((jumlahUangIN - jumlahUangOUT) / jumlahUangIN) * 100;
@@ -69,16 +73,32 @@ class App extends React.Component {
     const storedSummary = localStorage.getItem("summary");
 
     if (storedSummary) {
-      this.setState(JSON.parse(storedSummary) );
-  }else {
-    this.fnHitung()
-  }
+      this.setState(JSON.parse(storedSummary));
+    } else {
+      this.fnHitung();
+    }
   }
 
   componentDidUpdate() {
-    const { sisaUang, persentaseUang, pemasukanUang, pengeluaranUang, transaksiIN, transaksiOUT, summary } = this.state;
-  const stateToStore = { sisaUang, persentaseUang, pemasukanUang, pengeluaranUang, transaksiIN, transaksiOUT, summary };
-  localStorage.setItem("appState", JSON.stringify(stateToStore));
+    const {
+      sisaUang,
+      persentaseUang,
+      pemasukanUang,
+      pengeluaranUang,
+      transaksiIN,
+      transaksiOUT,
+      summary,
+    } = this.state;
+    const stateToStore = {
+      sisaUang,
+      persentaseUang,
+      pemasukanUang,
+      pengeluaranUang,
+      transaksiIN,
+      transaksiOUT,
+      summary,
+    };
+    localStorage.setItem("appState", JSON.stringify(stateToStore));
   }
   render() {
     return (
@@ -88,7 +108,9 @@ class App extends React.Component {
             <h1 className="fw-bold">FEEDUITEN APPS</h1>
             <hr className="w-75 mx-auto" />
             <h2 className="fw-bold">Rp. {this.state.sisaUang} ,-</h2>
-            <span className="title-sm">Sisa uang kamu tersisa {this.state.persentaseUang}% lagi</span>
+            <span className="title-sm">
+              Sisa uang kamu tersisa {this.state.persentaseUang}% lagi
+            </span>
           </div>
         </div>
 
@@ -101,7 +123,9 @@ class App extends React.Component {
               <span className="title-sm">Pemasukan</span>
               <h3 className="fw-bold">Rp. {this.state.pemasukanUang} ,-</h3>
               <div>
-                <span className="title-sm text-ungu fw-bold">{this.state.transaksiIN} </span>
+                <span className="title-sm text-ungu fw-bold">
+                  {this.state.transaksiIN}{" "}
+                </span>
                 <span className="title-sm"> Transaksi</span>
               </div>
             </div>
@@ -115,7 +139,9 @@ class App extends React.Component {
               <span className="title-sm">Pemakaian</span>
               <h3 className="fw-bold">Rp. {this.state.pengeluaranUang} ,-</h3>
               <div>
-                <span className="title-sm text-ungu fw-bold">{this.state.transaksiOUT} </span>
+                <span className="title-sm text-ungu fw-bold">
+                  {this.state.transaksiOUT}{" "}
+                </span>
                 <span className="title-sm"> Transaksi</span>
               </div>
             </div>
@@ -126,20 +152,50 @@ class App extends React.Component {
           <div className="col-12 d-flex align-items-center justify-content-between">
             <h4>Ringkasan Transaksi</h4>
             <div className="wrapper-button d-flex">
-              <ModalCreate action={this.tambahItem} category="IN" variant="button btn-ungu px-3 py-2 me-2 box-sh" text="Pemasukan" icon="bi bi-plus-circle ms-1" modalheading="Tambahkan Pemasukan" />
-              <ModalCreate action={this.tambahItem} category="OUT" variant="button btn-pink px-3 py-2 box-sh" text="Pengeluaran" icon="bi bi-dash-circle-dotted ms-1" modalheading="Tambahkan Pengeluaran" />
+              <ModalCreate
+                action={this.tambahItem}
+                category="IN"
+                variant="button btn-ungu px-3 py-2 me-2 box-sh"
+                text="Pemasukan"
+                icon="bi bi-plus-circle ms-1"
+                modalheading="Tambahkan Pemasukan"
+              />
+              <ModalCreate
+                action={this.tambahItem}
+                category="OUT"
+                variant="button btn-pink px-3 py-2 box-sh"
+                text="Pengeluaran"
+                icon="bi bi-dash-circle-dotted ms-1"
+                modalheading="Tambahkan Pengeluaran"
+              />
             </div>
           </div>
         </div>
 
         <div className="row mt-4">
-          { this.state.summary.length < 1 &&  <Alert />}
+          {this.state.summary.length < 1 && <Alert />}
           {this.state.summary.map((sum, index) => {
             return (
-              <div key={index} className="col-12 d-flex mb-3 justify-content-between align-items-center">
+              <div
+                key={index}
+                className="col-12 d-flex mb-3 justify-content-between align-items-center"
+              >
                 <div className="d-flex align-items-center">
-                  <div className={sum.category === "IN" ? "icon-wrapper-in box-sh" : "icon-wrapper-out box-sh"}>
-                    <i className={sum.category === "IN" ? "bi bi-wallet2" : "bi bi-bag-dash"} aria-hidden="true"></i>
+                  <div
+                    className={
+                      sum.category === "IN"
+                        ? "icon-wrapper-in box-sh"
+                        : "icon-wrapper-out box-sh"
+                    }
+                  >
+                    <i
+                      className={
+                        sum.category === "IN"
+                          ? "bi bi-wallet2"
+                          : "bi bi-bag-dash"
+                      }
+                      aria-hidden="true"
+                    ></i>
                   </div>
                   <div className="transaction ms-3 d-flex flex-column">
                     <h5>{sum.deskripsi} </h5>
@@ -147,7 +203,11 @@ class App extends React.Component {
                   </div>
                 </div>
                 {/* Conditional Rendering  untuk mengendalikan tampilan elemen di halaman berdasarkan kondisi tertentu.  */}
-                <h6 className={sum.category === "IN" ? "money-in" : "money-out"}>Rp. {sum.nominal} </h6>
+                <h6
+                  className={sum.category === "IN" ? "money-in" : "money-out"}
+                >
+                  Rp. {sum.nominal}{" "}
+                </h6>
               </div>
             );
           })}
@@ -156,7 +216,5 @@ class App extends React.Component {
     );
   }
 }
-
-
 
 export default App;
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,7 @@
 import React from "react";
 
-class Alert extends React.Component {
-    constructor() {
-      super();
-    }
-  
-    render() {
-      return <h1 className="text-center">Data Masih Kosong</h1>;
-    }
-  }
+const Alert = () => {
+  return <h1 className="text-center">Data Masih Kosong</h1>;
+};
 
-  export default Alert
+export default Alert;
